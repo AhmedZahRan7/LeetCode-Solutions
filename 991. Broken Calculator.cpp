7052ada85ea2b469b2989ea79c51370e84e50624
AhmedZahRan7 <ahmed.mimo720@gmail.com>
2021-02-21 11:16:02 +0200

    991

---
+++ 991. Broken Calculator.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int brokenCalc(int X, int Y) {
+        if(Y==X) return 0;
+        if(X>Y) return X-Y;
+        if(Y%2==0) return 1+brokenCalc(X,Y/2);
+        return 1+brokenCalc(X,Y+1);
+    }
+};

c7eefdcaaf99446aba3019b2ccdbcfb4d6261526
ahmedZahran7 <ahmed.mimo720@gmail.com>
2022-02-03 18:33:16 +0200

    454

---
+++ 454. 4Sum II.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        unordered_map<int,int> m;
+        for(int i:nums1) for(int j:nums2) m[i+j]++;
+        int sum = 0;
+        for(int i:nums3) for(int j:nums4) sum+=m[-1*(i+j)];
+        return sum;
+    }
+};
